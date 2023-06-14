eab475c36e3c5a9b2fe3ec907a1c80d0db56e279
19Urii91 <633mir@mail.ru>
2023-06-14 09:22:07 +0800

    Доделывает вторую домашку

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,27 @@
 // Объявление функции для проверки длины строки:
-const exercise = 'Второе задание по JS';
+
 const checkLengthString = function (string, length) {
   if (string.length <= length) {
     return true;
+  } else {
+    return false;
   }
 };
 
 // Вызов функции для проверки длины строки:
 
-checkLengthString(exercise, 30);
+checkLengthString('Второе задание по JS', 20);
 
 // Объявление функции для проверки, является ли строка палиндромом:
 
-const checkPalindromeString = (palindrome) => {
-  if (palindrome) {
+const checkStringPalindrome = (palindrome) => {
+  if (palindrome.toLowerCase() === palindrome.toLowerCase().split('').reverse().join('')) {
     return true;
+  } else {
+    return false;
   }
 };
 
 // Вызов функции для проверки, является ли строка палиндромом:
 
-checkPalindromeString('доход');
+checkStringPalindrome('доход');
